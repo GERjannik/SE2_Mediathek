02772c91f4c9176975f5a85ec9044c6cca63c315
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2017-03-24 08:49:28 +0100

    First commit of code

---
+++ src/main/java/de/hdm_stuttgart/se2/softwareProject/mediathek/Movie.java
@@ -0,0 +1,18 @@
+package de.hdm_stuttgart.se2.softwareProject.mediathek;
+
+import java.util.Date;
+
+public class Movie {
+
+	private String title;
+	private String[] actor;
+	private int duration;
+	private Date releaseDate;
+	private String regisseur;
+	private boolean favorite;
+	private String genre;
+	private String info;
+	private int ranking;
+	private int[] inPlaylists;
+	
+}
