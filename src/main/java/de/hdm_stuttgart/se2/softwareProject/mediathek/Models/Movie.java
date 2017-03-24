c9404156aa68ad4e5c00d87abf196aa0778affe2
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2017-03-24 09:44:31 +0100

    Erste Methode in Klasse Movie

---
+++ src/main/java/de/hdm_stuttgart/se2/softwareProject/mediathek/Models/Movie.java
@@ -0,0 +1,40 @@
+package de.hdm_stuttgart.se2.softwareProject.mediathek.Models;
+
+import java.io.File;
+import java.util.Date;
+import java.util.List;
+
+public class Movie {
+
+	private String title;
+	private List<String> actor;
+	private int duration;
+	private Date releaseDate;
+	private String regisseur;
+	private boolean favorite;
+	private String genre;
+	private String info;
+	private int ranking;
+	private List<Integer> inPlaylists;
+	private File file;
+	
+	private void showDetails(Movie m) {
+		System.out.println(m.title + "\nSchauspieler:");
+		// Schleife, damit alle Schauspieler, die das Array enthält, ausgegeben werden
+		for (String i : m.actor) {
+			System.out.println(i);
+		}
+		System.out.println("Dauer: " + m.duration);
+		System.out.println("Erscheinungsdatum: " + m.releaseDate);
+		System.out.println("Regisseur: " + m.regisseur);
+		System.out.println("Genre: " + m.genre);
+		System.out.println("Filmbeschreibung: " + m.info);
+		System.out.println("Bewertung: " + m.ranking);
+	}
+
+
+	
+	
+	
+	
+}
